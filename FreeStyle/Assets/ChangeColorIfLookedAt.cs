4068014745c712b2b218bf8780c2e3f37db409a7
Noah Pena <noahpena7@utexas.edu>
2016-02-04 16:55:47 -0600

    First Commit

---
+++ FreeStyle/Assets/ChangeColorIfLookedAt.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColorIfLookedAt : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 moveDirection = Camera.main.transform.forward;
+
+		Debug.Log (moveDirection);
+
+		RaycastHit hit;
+		Vector3 CameraCenter = Camera.main.ScreenToWorldPoint (new Vector3 (Screen.width / 2, Screen.height / 2, Camera.main.nearClipPlane));
+		if (Physics.Raycast(CameraCenter,  Camera.main.transform.forward, out hit, 100))
+		{
+			if(Camera.main.transform.forward.y >= .9)
+			{
+				GameObject hitObject = hit.transform.gameObject;
+					
+				if(hitObject == gameObject)
+				{
+					gameObject.GetComponent<Renderer> ().material.color = Color.blue;
+				}
+			}
+			else
+			{
+				gameObject.GetComponent<Renderer>().material.color = Color.white;
+			}
+		}
+
+	}
+}
---
+++ FreeStyle/Assets/TeleportPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportPlayer : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.P)) 
+		{
+			Debug.Log ("Pressed");
+			this.transform.position.Set (this.transform.position.x, this.transform.position.y + 100, this.transform.position.z);
+
+		}
+	}
+}
