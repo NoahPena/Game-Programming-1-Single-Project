5fd8d7c3877e3b840758be88199cde02cd889d49
Noah Pena <noahpena7@utexas.edu>
2016-03-04 23:58:29 -0600

    Created First Puzzle and Fixed Flipping Mechanic

--- FreeStyle/Assets/FlipOrientation.cs
+++ FreeStyle/Assets/FlipOrientation.cs
@@ -31,7 +31,7 @@ public class FlipOrientation : MonoBehaviour
 		    && Physics.Raycast (botLeft, out botLeftHit) && Physics.Raycast (botRight, out botRightHit)) {
 			Debug.Log (Camera.main.transform.forward.y);
 
-			if ((Camera.main.transform.forward.y >= .9) && grounded) {
+			if ((Camera.main.transform.forward.y >= .9) && grounded && gameObject.GetComponent<RigidBodyFPSWalker>().gravity == 10f) {
 				GameObject topLeftObject = topLeftHit.transform.gameObject;
 				GameObject topRightObject = topRightHit.transform.gameObject;
 				GameObject botRightObject = botRightHit.transform.gameObject;
@@ -41,10 +41,11 @@ public class FlipOrientation : MonoBehaviour
 				if (topLeftObject == topRightObject && topRightObject == botRightObject && botRightObject == botLeftObject && botLeftObject == middleObject) {
 					gameObject.GetComponent<RigidBodyFPSWalker> ().gravity = -10f;
 
-					middleObject.GetComponent<Renderer> ().material.color = Color.blue;
-					gameObject.transform.position = new Vector3 (gameObject.transform.position.x, 13, gameObject.transform.position.z);
+					//middleObject.GetComponent<Renderer> ().material.color = Color.blue;
+					gameObject.transform.position = new Vector3 (gameObject.transform.position.x, (middleObject.transform.position.y - (gameObject.GetComponent<CapsuleCollider>().height/2) - 
+						gameObject.transform.position.y) + gameObject.transform.position.y, gameObject.transform.position.z);
 				}
-			} else if ((Camera.main.transform.forward.y <= -.9) && grounded) {
+			} else if ((Camera.main.transform.forward.y <= -.9) && grounded && gameObject.GetComponent<RigidBodyFPSWalker>().gravity == -10f) {
 				GameObject topLeftObject = topLeftHit.transform.gameObject;
 				GameObject topRightObject = topRightHit.transform.gameObject;
 				GameObject botRightObject = botRightHit.transform.gameObject;
@@ -54,8 +55,9 @@ public class FlipOrientation : MonoBehaviour
 				if (topLeftObject == topRightObject && topRightObject == botRightObject && botRightObject == botLeftObject && botLeftObject == middleObject) {
 					gameObject.GetComponent<RigidBodyFPSWalker> ().gravity = 10f;
 
-					middleObject.GetComponent<Renderer> ().material.color = Color.blue;
-					gameObject.transform.position = new Vector3 (gameObject.transform.position.x, 13, gameObject.transform.position.z);
+					//middleObject.GetComponent<Renderer> ().material.color = Color.blue;
+					gameObject.transform.position = new Vector3 (gameObject.transform.position.x, (gameObject.transform.position.y + (gameObject.GetComponent<CapsuleCollider>().height/2) +
+						middleObject.transform.position.y) - gameObject.transform.position.y, gameObject.transform.position.z);
 				}
 			}
 		}
