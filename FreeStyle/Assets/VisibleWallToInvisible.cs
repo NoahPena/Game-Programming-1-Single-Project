2a9d640c2a196a3f81af2b9a23ad6a7a6e633375
Noah Pena <noahpena7@utexas.edu>
2016-03-05 21:01:46 -0600

    It's amazing what some lines can do to a person's perspective on life

---
+++ FreeStyle/Assets/VisibleWallToInvisible.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisibleWallToInvisible : MonoBehaviour 
+{
+
+	public GameObject lookObject;
+	public GameObject actionObject;
+	GameObject player;
+	bool once = false;
+
+	// Use this for initialization
+	void Start () 
+	{
+		player = GameObject.Find ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 moveDirection = Camera.main.transform.forward;
+
+		RaycastHit hit;
+		Vector3 cameraCenter = Camera.main.ScreenToWorldPoint (new Vector3 (Screen.width / 2, Screen.height / 2, Camera.main.nearClipPlane));
+
+		if (Physics.Raycast (cameraCenter, moveDirection, out hit, 100)) 
+		{
+			if (hit.transform.gameObject == lookObject && !once && player.transform.position.x < actionObject.transform.position.x) 
+			{
+				once = true;
+
+				actionObject.SetActive (false);
+			}
+		}
+	}
+}
